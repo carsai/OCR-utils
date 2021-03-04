6de7a89290c054f3608d43304301ea075738c85f
Pablo <psgh1989@gmail.com>
2021-03-04 19:45:04 +0100

    Se añade ficheros de logs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const upload = require('multer')()
 require('dotenv').config({ path: (process.env.NODE_ENV) ? `.env.${process.env.NODE_ENV}` : '.env' })
 const morgan = require('morgan')
+const fs = require('fs')
+const path = require('path')
 
 const app = express()
 
@@ -12,9 +14,13 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 if (process.env.LOG_LEVEL) {
+  let accessLogStream = null
+  if (process.env.LOG_FICHERO === 'S') {
+    accessLogStream = fs.createWriteStream(path.join(__dirname, 'log', 'api.log'), { flags: 'a' })
+  }
   morgan.token('params', req => req.body)
   morgan.token('files', req => req.files)
-  app.use(morgan(process.env.LOG_LEVEL))
+  app.use(morgan(process.env.LOG_LEVEL, { stream: accessLogStream }))
 }
 
 app.use(require('./routes/index'))
